5bf01bf9257839518c28006ac87a25371ace9fd7
ricky-bruner <alteredecho07@yahoo.com>
2018-07-27 23:55:48 -0500

    restructured datastructure create function for universal use

--- js/debbiedata.js
+++ js/debbiedata.js
@@ -1,26 +1,27 @@
 
-function makeCakeObject(name, packaging, description, amount, image){
-    const snackCake = {
+const debbieCakes = [];
+function makeCakeObject(snackCake, name, packaging, description, amount, image){
+    snackCake = {
         name: name,
         packaging: packaging,
         description: description,
         amount: amount,
         image: image
     }
-    return snackCake 
+    debbieCakes.push(snackCake);
+    return snackCake
 }
 
 
-const starCrunch = makeCakeObject("Star Crunch", "Individually Wrapped", "A soft, chewy cookie topped with caramel and crispy rice then covered in a layer of fudge .", 12, "../images/starcrunch.jpg");
+makeCakeObject("starCrunch", "Star Crunch", "Individually Wrapped", "A soft, chewy cookie topped with caramel and crispy rice then covered in a layer of fudge .", 12, "../images/starcrunch.jpg");
 
-const swissCakeRoll = makeCakeObject("Swiss Cake Roll", "Twin Wrapped", "Chocolate cake rolled around a layer of creme filling and drenched with fudge coating.", 12, "../images/swisscakeroll.jpg");
+makeCakeObject("swissCakeRoll", "Swiss Cake Roll", "Twin Wrapped", "Chocolate cake rolled around a layer of creme filling and drenched with fudge coating.", 12, "../images/swisscakeroll.jpg");
 
-const oatmealCremePie = makeCakeObject("Oatmeal Creme Pie", "Individually Wrapped", "Soft oatmeal cookie patties held together by a sweet creme middle", 12, "../images/oatmealcremepie.jpg");
+makeCakeObject("oatmealCremePie", "Oatmeal Creme Pie", "Individually Wrapped", "Soft oatmeal cookie patties held together by a sweet creme middle", 12, "../images/oatmealcremepie.jpg");
 
-const nuttyBar = makeCakeObject("Nutty Bars", "Twin Wrapped", "Classic crunchy wafer bars layered with peanut butter and covered in rich chocolate fudge.", 12, "../images/nuttybar.jpg");
+makeCakeObject("nuttyBars", "Nutty Bars", "Twin Wrapped", "Classic crunchy wafer bars layered with peanut butter and covered in rich chocolate fudge.", 12, "../images/nuttybar.jpg");
 
-const debbieCakes = [];
-debbieCakes.push(starCrunch, swissCakeRoll, oatmealCremePie, nuttyBar);
+// debbieCakes.push(starCrunch, swissCakeRoll, oatmealCremePie, nuttyBar);
 
 function setData(nameTag, jsObject){
     let stringifiedObject = JSON.stringify(jsObject);
@@ -28,7 +29,7 @@ function setData(nameTag, jsObject){
 }
 
 function populateDB(){
-    setData("Little Debbie Snacks", debbieCakes);
+    setData("littleDebbieSnacks", debbieCakes);
     mainContent.removeChild(loadButton);
     mainContent.innerHTML += 
     `<button id="show-snacks">Show The Schnacks</button>`
