cedfdfb3facf8cd3136791cf30d3df8d3e1b6a38
ricky-bruner <alteredecho07@yahoo.com>
2018-07-28 01:51:22 -0500

    fixed debbie logo link

--- js/main.js
+++ js/main.js
@@ -97,7 +97,7 @@ function popSnacks(){
     mainContent.removeChild(showSnacks);
     mainContent.innerHTML +=
         `<header>
-            <img src="https://www.littledebbie.com/images/template/logo-home.pnggs" alt="Little Debbie Logo">
+            <img src="https://vignette.wikia.nocookie.net/peanuts/images/9/96/LittleDebbieLogo.png/revision/latest?cb=20160714075106" alt="Little Debbie Logo">
             <h2>SCHNAAAAAAKS!</h2>
         </header>
         <div class="snack-girl">
